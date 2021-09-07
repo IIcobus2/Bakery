dde1e5920ff7564d4aec478ae6e83a700a47c7c6
IIcobus2 <walter_felizardo@live.com>
2021-09-07 18:44:15 -0300

    07/09

---
+++ force-app/main/default/aura/checkOutApp/checkOutApp.app
@@ -0,0 +1,4 @@
+<aura:application access="global" extends="ltng:outApp" implements="force:hasRecordId">
+    <aura:attribute name="recordId" type="String"/>  
+    <c:checkOut></c:checkOut>
+</aura:application>
--- force-app/main/default/lwc/checkOut/checkOut.js
+++ force-app/main/default/lwc/checkOut/checkOut.js
@@ -1,5 +1,6 @@
 import { LightningElement, api, track, wire } from 'lwc';
 import findRecords from '@salesforce/apex/LwcLookUpController.findRecords'
+import getClient from '@salesforce/apex/LwcLookUpController.getClient'
 
 const columns = [
     { label: 'ID', fieldName: 'id' },
@@ -9,9 +10,30 @@ const columns = [
 export default class BasicDatatable extends LightningElement {
     columns = columns;
     lookUpProduct = '';
+    title = '';
+    id = 0;
+    @api recordId;
+    @track product = '';
+    @track searchProduct;
     @track optionsProduct = [];
+    @track clientInfo = [];
+
+    
+
+    displayCombobox(){
+        this.template.querySelector('[data-js="showCombobox"]').className = "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-is-open";
+    }
+
+    hideCombobox(){
+        this.template.querySelector('[data-js="showCombobox"]').className = "slds-combobox slds-dropdown-trigger slds-dropdown-trigger_click slds-is-closed";
+    }
 
     handleInputChange(evt) {
+        if(evt.target.value === ''){
+            this.hideCombobox();
+        }else{
+            this.displayCombobox();
+        }
         this.lookUpProduct = evt.target.value;
         console.log(this.lookUpProduct); 
     }
@@ -27,12 +49,25 @@ export default class BasicDatatable extends LightningElement {
     }
 
     itemChosed(evt){
-        let searchProduct = this.template.querySelector('[data-js="searchProduct"]');
-        (searchProduct.value) = (evt.target.outerText);
+        this.searchProduct = this.template.querySelector('[data-js="searchProduct"]');
+        this.searchProduct.value = (evt.target.outerText);
+        this.lookUpProduct = this.searchProduct.value;
+    }
+
+    @wire(getClient, {recordID:'$recordId'})
+        retrievedClient({error,data}){
+            if(data){
+                this.clientInfo = data;
+                this.title = (this.clientInfo.typeClient).toUpperCase()+" "+(this.clientInfo.name).toUpperCase();
+            }else if(error){
+                console.log(error);
+            }
+        }
+    connectedCallback(){
+        
     }
 
     addItem(){
-            
-        
+        // chamar classe e passar todos os dados bonitinhos em JSON incluindo direto no datatable!!!!!
     }
 }
